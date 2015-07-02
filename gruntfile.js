47cee9b75a75d8672006cb6a49b8cf612187a9d3
Mike Easley <measley@1800contacts.com>
2015-07-02 11:30:18 -0600

    added grunt plugin to autostart selenium locally

--- gruntfile.js
+++ gruntfile.js
@@ -13,10 +13,10 @@
             	}
             }
         });
-
+        grunt.loadNpmTasks('grunt-selenium-webdriver');
         grunt.loadNpmTasks('intern');
 
         grunt.registerTask('test', [ 'intern' ]);
-        grunt.registerTask('default', [ 'intern' ]);
+        grunt.registerTask('default', ['selenium_start', 'intern', 'selenium_stop' ]);
     };
 }());

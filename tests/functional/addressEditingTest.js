40347636aeb1d0d481767e4b4187ac8a3b05bd66
Steven Ulibarri <steven.ulibarri@gmail.com>
2015-07-13 16:25:58 -0600

    refactored several parts of address editing test to addressCommand, standardized casing for generated customer objects, added clear form to baseCommand, account page can be deleted

--- tests/functional/addressEditingTest.js
+++ tests/functional/addressEditingTest.js
@@ -2,21 +2,19 @@ define([
     'intern!object',
     '../utility/generator',
     '../config',
-    './pages/accountPage',
-    './pages/header'
+    './customCommands/addressCommand'
 ],
-function (registerSuite, generator, config, AccountPage, Header) {
+function (registerSuite, generator, config, AddressCommand) {
     registerSuite(function(){
         var customer;
-        var accountPage;
-        var header;
+        var addressCommand;
 
         return {
-            name: 'Sign in from account page',
+            name: 'Edit Address',
             setup: function(){
                 customer = generator.getExistingCustomer(config.existingId);
-                accountPage = new AccountPage(this.remote);
-                header = new Header(this.remote);
+                addressCommand = new AddressCommand(this.remote);
+
                 return this.remote
                 .clearCookies()
                 .setTimeout('script', 60000)
@@ -25,81 +23,83 @@ function (registerSuite, generator, config, AccountPage, Header) {
                 .get(config.URL + '/account');
             },
 
-            'test account page login' : function() {
-                return accountPage.login(customer);
+            'login' : function() {
+                return addressCommand.login(customer);
             },
 
-            'go to account from header' : function() {
-                return header.goToAccount();
+            'navigate to account page' : function() {
+                return addressCommand.findAndClick('a[title="My Account"]');
             },
 
-            'click address tab from account page' : function() {
-                return accountPage.clickAccountAddressTab();
+            'navigate to account page address tab' : function() {
+                return addressCommand.findAndClick('a[href$="1800contacts/address-list"]');
             },
 
             'click address edit' : function() {
-                return accountPage.editAddress();
+                return addressCommand.findAndClick('a[href*="Address-Edit"]');
             },
 
-            'clear first name form field' : function() {
-                return accountPage.clearAddressField('dwfrm_profile_address_firstname');
-            },
-
-            'fill out first name field' : function() {
-                return accountPage.fillAddressField('Smith', 'dwfrm_profile_address_firstname');
-            },
-
-            'clear last name form field' : function() {
-                return accountPage.clearAddressField('dwfrm_profile_address_lastname');
-            },
-
-            'fill out last name field' : function() {
-                return accountPage.fillAddressField('Johnson', 'dwfrm_profile_address_lastname');
-            },
-
-            'clear address1 form field' : function() {
-                return accountPage.clearAddressField('dwfrm_profile_address_address1');
-            },
-
-            'fill out address1 field' : function() {
-                return accountPage.fillAddressField('321 Pecirt Yaw', 'dwfrm_profile_address_address1');
-            },
-
-            'clear address2 form field' : function() {
-                return accountPage.clearAddressField('dwfrm_profile_address_address2');
-            },
-
-            'fill out address2 field' : function() {
-                return accountPage.fillAddressField('Apt.#101', 'dwfrm_profile_address_address2');
-            },
-
-            'clear city form field' : function() {
-                return accountPage.clearAddressField('dwfrm_profile_address_city');
-            },
-
-            'fill out city field' : function() {
-                return accountPage.fillAddressField('Draper', 'dwfrm_profile_address_city');
-            },
-
-            'clear zip form field' : function() {
-                return accountPage.clearAddressField('dwfrm_profile_address_zip');
-            },
-
-            'fill zip city field' : function() {
-                return accountPage.fillAddressField('49048', 'dwfrm_profile_address_zip');
-            },
-
-            'clear phone form field' : function() {
-                return accountPage.clearAddressField('dwfrm_profile_address_phone');
-            },
-
-            'fill phone city field' : function() {
-                return accountPage.fillAddressField('(456) 456-4646', 'dwfrm_profile_address_phone');
-            },
-
-            'save address form' : function() {
-                return accountPage.saveAddressForm();
+            'fill out address form' : function() {
+                return addressCommand.fillAddressForm(customer)
+                .sleep(5000);
+                // return accountPage.clearAddressField('dwfrm_profile_address_firstname');
             }
+
+            // 'fill out first name field' : function() {
+            //     return accountPage.fillAddressField('Smith', 'dwfrm_profile_address_firstname');
+            // },
+
+            // 'clear last name form field' : function() {
+            //     return accountPage.clearAddressField('dwfrm_profile_address_lastname');
+            // },
+
+            // 'fill out last name field' : function() {
+            //     return accountPage.fillAddressField('Johnson', 'dwfrm_profile_address_lastname');
+            // },
+
+            // 'clear address1 form field' : function() {
+            //     return accountPage.clearAddressField('dwfrm_profile_address_address1');
+            // },
+
+            // 'fill out address1 field' : function() {
+            //     return accountPage.fillAddressField('321 Pecirt Yaw', 'dwfrm_profile_address_address1');
+            // },
+
+            // 'clear address2 form field' : function() {
+            //     return accountPage.clearAddressField('dwfrm_profile_address_address2');
+            // },
+
+            // 'fill out address2 field' : function() {
+            //     return accountPage.fillAddressField('Apt.#101', 'dwfrm_profile_address_address2');
+            // },
+
+            // 'clear city form field' : function() {
+            //     return accountPage.clearAddressField('dwfrm_profile_address_city');
+            // },
+
+            // 'fill out city field' : function() {
+            //     return accountPage.fillAddressField('Draper', 'dwfrm_profile_address_city');
+            // },
+
+            // 'clear zip form field' : function() {
+            //     return accountPage.clearAddressField('dwfrm_profile_address_zip');
+            // },
+
+            // 'fill zip city field' : function() {
+            //     return accountPage.fillAddressField('49048', 'dwfrm_profile_address_zip');
+            // },
+
+            // 'clear phone form field' : function() {
+            //     return accountPage.clearAddressField('dwfrm_profile_address_phone');
+            // },
+
+            // 'fill phone city field' : function() {
+            //     return accountPage.fillAddressField('(456) 456-4646', 'dwfrm_profile_address_phone');
+            // },
+
+            // 'save address form' : function() {
+            //     return accountPage.saveAddressForm();
+            // }
 
             //'logout from header' : function() {
             //    return header.logout(customer);
--- tests/functional/customCommands/addressCommand.js
+++ tests/functional/customCommands/addressCommand.js
@@ -1,19 +1,35 @@
 define([
 	'./BaseCommand'
-]
-function (BaseCommand) {
+],
 
-var proto = addressCommand.prototype = Object.create(BaseCommand.prototype, {});
+function(BaseCommand) {
 
-function AddressCommand() {
-	BaseCommand.apply(this, arguments);
-}
+	var proto = AddressCommand.prototype = Object.create(BaseCommand.prototype, {});
 
-proto.constructor = AddressCommand;
+	function AddressCommand() {
+		BaseCommand.apply(this, arguments);
+	}
 
+	proto.constructor = AddressCommand;
 
+	proto.fillAddressForm = function(customer) {
+		return new this.constructor(this, function() {
+			console.log(customer);
+			return this.parent
+			.clearForm('#edit-address-form')
+			.sleep(1000)
+			.end()
+			.enterInput('#dwfrm_profile_address_address1', customer.shipping_address1)
+			.enterInput('#dwfrm_profile_address_address2', customer.shipping_address2)
+			.enterInput('#dwfrm_profile_address_city',  customer.shipping_city)
+			.enterInput('#dwfrm_profile_address_zip', customer.shipping_zip)
+			.enterInput('#dwfrm_profile_address_phone', customer.shipping_phone)
+			.enterInput('#dwfrm_profile_address_firstname', customer.firstName)
+			.enterInput('#dwfrm_profile_address_lastname', customer.lastName)
+		});
+	};
 
-return AddressCommand;
+	return AddressCommand;
 });
 
 
